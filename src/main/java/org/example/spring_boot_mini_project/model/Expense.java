6a1e7582e21a929d18b0a5cd1b89492806e3e2bf
RITHYHONG <rithyhong8998@gmail.com>
2024-04-17 10:27:23 +0700

    almost done with expense Post

--- src/main/java/org/example/spring_boot_mini_project/model/Expense.java
+++ src/main/java/org/example/spring_boot_mini_project/model/Expense.java
@@ -1,20 +1,36 @@
 package org.example.spring_boot_mini_project.model;
 
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class Expense {
+
     private UUID expenseId;
-    private int amount;
+
+    @NotNull(message = "Amount is required")
+    private BigDecimal amount;
+
+    @NotBlank(message = "Description is required")
     private String description;
-    private Date date;
-    private UUID userId;
-    private UUID categoryId;
+
+    @NotNull(message = "Date is required")
+    private LocalDateTime expense_date;
+
+    @NotNull(message = "User ID is required")
+    private UUID user_id;
+
+    @NotNull(message = "Category ID is required")
+    private UUID category_id;
 }
+
